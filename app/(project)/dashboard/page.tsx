8c59f857a4fe1ec3d77d75a7f389552080dadd01
DandanLeinad <danieldias6788@gmail.com>
2025-04-14 17:31:46 -0300

    feat: refatorar autenticação para gerenciar login e logout no dashboard

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { handleAuth } from "@/app/actions/handle-auth";
 import { auth } from "@/app/lib/auth";
 import { redirect } from "next/navigation";
 
@@ -10,11 +11,21 @@ export default async function Dashboard() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col gap-10 items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Protect Dashboard Page</h1>
       <p>
         {session?.user?.email ? session.user.email : "Usuário não autenticado"}
       </p>
+      {session?.user?.email && (
+        <form action={handleAuth}>
+          <button
+            type="submit"
+            className="border rounded-md px-2 cursor-pointer"
+          >
+            Logout
+          </button>
+        </form>
+      )}
     </div>
   );
 }
