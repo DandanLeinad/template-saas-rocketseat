8d6b26227ffd9ad790130ca225bef698735b5af1
DandanLeinad <danieldias6788@gmail.com>
2025-04-14 17:11:32 -0300

    feat: implementar proteção na página do dashboard e exibir email do usuário autenticado

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,7 +1,20 @@
-export default function Dashboard() {
+import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  // Estamos no lado do servidor, então não podemos usar o useSession aqui
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Protect Dashboard Page</h1>
+      <p>
+        {session?.user?.email ? session.user.email : "Usuário não autenticado"}
+      </p>
     </div>
   );
 }
