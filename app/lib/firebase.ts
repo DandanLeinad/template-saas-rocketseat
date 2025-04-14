8e581488ed39b07a4a1cd1d9aeaa8967ccc14737
DandanLeinad <danieldias6788@gmail.com>
2025-04-14 19:33:15 -0300

    feat: inicializar configuração do Firebase e exportar Firestore

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,1 +1,18 @@
+import { cert, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+// import { getStorage } from "firebase-admin/storage";
 import "server-only";
+
+const firebaseCert = cert({
+  projectId: "your-project-id",
+  clientEmail: "your-client-email",
+  privateKey: "your-private-key",
+});
+
+export const adminApp = initializeApp({
+  credential: firebaseCert,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+});
+
+export const db = getFirestore();
+// export const storage = getStorage().bucket();
