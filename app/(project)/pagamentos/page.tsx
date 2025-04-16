858b8246deb845dee1105297c254f2dbe9d3c686
DandanLeinad <danieldias6788@gmail.com>
2025-04-16 11:05:04 -0300

    feat: adiciona página de pagamentos com opções para criar pagamento, assinatura e portal de pagamentos

---
+++ app/(project)/pagamentos/page.tsx
@@ -0,0 +1,14 @@
+export default function Pagamentos() {
+  return (
+    <div className="flex flex-col gap-10 items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Pagamentos</h1>
+      <button className="border rounded-md px-1">Criar Pagamento Stripe</button>
+      <button className="border rounded-md px-1">
+        Criar Assinatura Stripe
+      </button>
+      <button className="border rounded-md px-1">
+        Criar Portal de pagamentos
+      </button>
+    </div>
+  );
+}
